2827f7eeb19ddb171bf35e41c1a6296e79995d6c
chenfengjin <1871653365@qq.coom>
2021-04-08 13:02:14 +0800

    RFC: A MVP for xuper-sdk-py

--- xuperchain/code_service.py
+++ xuperchain/code_service.py
@@ -1,7 +1,6 @@
 from xuperchain.context import Context
 from xuperchain.contract import contract_pb2
 from datetime import datetime
-from xuperchain.exception import XuperException
 
 import logging
 
@@ -15,7 +14,7 @@ class NativeCodeServicer(object):
         self.lastPing = datetime.now()
         self.channel = channel
 
-    def Call(self, request: contract_pb2.NativeCallRequest, ctx):
+    def Call(self, request: contract_pb2.NativeCallRequest, grpcctx):
         """Missing associated documentation comment in .proto file."""
         ctxid = request.ctxid
         ctx = Context(ctxid=ctxid, channel=self.channel)
@@ -25,35 +24,24 @@ class NativeCodeServicer(object):
 
         if not hasattr(self.contract, method):
             found = False
+
         f = getattr(self.contract, method)
         if not f.__name__ == "contract_method_wraper":
             found = False
+
         if not found:
             resp = contract_pb2.Response(status=500, message="method {} not found".format(method), body=None)
             ctx.SetOutput(resp)
             return contract_pb2.NativeCallResponse()
         try:
-            out = f(ctx)
-            if type(out) == type(""):
-                out = out.encode()
-
-            if not type(out) == type(bytes("", "UTF-8")):
-                import json
-                # TODO @fegjin
-                out = json.dumps(out).encode()
-            resp = contract_pb2.Response(status=200, message=None, body=out)
-
+            resp = f(ctx)
+            # print(resp.body)
+            resp = contract_pb2.Response(status=resp.status, message=resp.msg, body=resp.body.encode())
             ctx.SetOutput(resp)
-
         except Exception as e:
-            if isinstance(e, XuperException):
-                status = 501
-                msg = e.msg
-            else:
-                status = 502
-
-                logging.exception(e)
-                msg = "method:{},msg:{}".format(method,str(e))  # error message should not be longer than 1000, which may cause problems
+            status = 500
+            logging.exception(e)
+            msg = "method:{},msg:{}".format(method, str(e))
             resp = contract_pb2.Response(status=status, message=msg, body=None)
             ctx.SetOutput(resp)
         return contract_pb2.NativeCallResponse()
